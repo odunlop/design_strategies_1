1e3fc7ccd25ea946d7dfa1b0fe3cbea451854fb5
Orla <orladunlop2@gmail.com>
2021-12-15 11:05:55 +0000

    Refactorising the rspec files

--- spec/banking_app_spec.rb
+++ spec/banking_app_spec.rb
@@ -3,21 +3,18 @@ require 'bank_account'
 describe BankAccount do
 
   it "can let users deposit money into their account" do
-    bank_account = BankAccount.new
-    bank_account.deposit(100)
-    expect(bank_account.view_balance).to eq "£100"
+    subject.deposit(100)
+    expect(subject.view_balance).to eq "£100"
   end
 
   it "can let users withdraw money from their account" do
-    bank_account = BankAccount.new
-    bank_account.deposit(100)
-    bank_account.withdraw(50)
-    expect(bank_account.view_balance).to eq "£50"
+    subject.deposit(100)
+    subject.withdraw(50)
+    expect(subject.view_balance).to eq "£50"
   end
-  
+
   it "can let users view their bank balance" do
-    bank_account = BankAccount.new
-    bank_account.deposit(100)
-    expect(bank_account.view_balance).to eq "£100"
+    subject.deposit(100)
+    expect(subject.view_balance).to eq "£100"
   end
 end
